78e58caedeec50e17d746c45baa33dea1e65ef71
yoann;^) <yoann.dossou@epitech.eu>
2025-08-15 18:31:46 +0100

    correction of the IDisplay

--- include/IGraphic.hpp
+++ include/IGraphic.hpp
@@ -5,7 +5,7 @@
 
 class IDisplayModule {
 public :
-    virtual ~ IDisplayModule() = default;
+    virtual ~IDisplayModule() = default;
     virtual void init() = 0;
     virtual void stop() = 0;
     virtual const std::string &getName() const = 0;

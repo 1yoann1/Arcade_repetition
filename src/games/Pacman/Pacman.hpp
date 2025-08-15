a2b4aeb147e317c0816f1bbd38727d562efd0750
yoann;^) <yoann.dossou@epitech.eu>
2025-08-15 07:51:02 +0100

    adding enum to Pacman

--- src/games/Pacman/Pacman.hpp
+++ src/games/Pacman/Pacman.hpp
@@ -1,5 +1,14 @@
 #include "../include/IGame.hpp"
 
+enum class Input {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    REFRESH,
+    NONE
+};
+
 class Pacman : public IGame {
 public:
     void init() override;

75a0708c49dbd8963df9d74f8fcf8a429dcfdeb6
yoann;^) <yoann.dossou@epitech.eu>
2025-08-15 08:04:21 +0100

    adding of some elements concerning the player

--- src/games/Pacman/Pacman.hpp
+++ src/games/Pacman/Pacman.hpp
@@ -9,6 +9,10 @@ enum class Input {
     NONE
 };
 
+struct Position {
+    int x, y;
+};
+
 class Pacman : public IGame {
 public:
     void init() override;
@@ -23,6 +27,8 @@ public:
 private:
     Input currentDirection;
     std::vector<std::string> mapData;
+    std::deque<Position> player;
+    int playerXstart, playerYstart;
     int HEIGHT, WIDTH;
     int scores;
 };
